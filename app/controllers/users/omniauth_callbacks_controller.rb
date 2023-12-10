aa18dc3bb012ad9dbd68e554f7782723be8a3f44
Mutuba <danielmutubait@gmail.com>
2023-12-10 12:06:59 +0300

    Initial commit for Bookme app

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -25,6 +25,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       flash[:success] = t 'devise.omniauth_callbacks.success', kind: 'Google'
       sign_in_and_redirect user, event: :authentication
     else
+      session['devise.google_data'] = request.env['omniauth.auth'].except('extra')
       flash[:alert] =
         t 'devise.omniauth_callbacks.failure', kind: 'Google', reason: "#{auth.info.email} is not authorized."
       redirect_to new_user_session_path
