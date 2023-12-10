a9225bc27cf55922f5876b98e1d58add02d0d495
Mutuba <danielmutubait@gmail.com>
2023-12-10 12:14:42 +0300

    do some clean up

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,27 +1,11 @@
-# # app/controllers/users/omniauth_callbacks_controller.rb
-# class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
-#   def google_oauth2
-#     @user = User.from_omniauth(request.env['omniauth.auth'])
-
-#     if @user.persisted?
-#       sign_in_and_redirect @user, event: :authentication
-#     else
-#       session['devise.google_data'] = request.env['omniauth.auth']
-#       redirect_to new_user_registration_url
-#     end
-
-#     # Access token
-#     access_token = request.env['omniauth.auth'].credentials.token
-#     # Now you can use 'access_token' to make requests on behalf of the user.
-#   end
-# end
+# app/controllers/users/omniauth_callbacks_controller.rb
 
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def google_oauth2
     user = User.from_omniauth(auth)
     if user.persisted?
       sign_out_all_scopes
-      request.env['omniauth.auth'].credentials.token
+      # request.env['omniauth.auth'].credentials.token
       flash[:success] = t 'devise.omniauth_callbacks.success', kind: 'Google'
       sign_in_and_redirect user, event: :authentication
     else
